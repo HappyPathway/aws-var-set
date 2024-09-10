ab6eca2f29765d484db6f898ef8364210b4391fa
David Arnold <10138997+djaboxx@users.noreply.github.com>
2024-09-10 13:50:02 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,17 +5,11 @@ locals {
   ]
 }
 
-module "var" {
-  for_each = toset(local.aws_secrets)
-  source   = "HappyPathway/var/env"
-  env_var  = each.value
-}
-
-resource "github_actions_organization_secret" "org_secret" {
-  for_each = tomap({
-    for secret in local.aws_secrets : secret => module.var[secret].value
-  })
-  secret_name     = each.key
-  visibility      = "private"
-  plaintext_value = each.value
+resource null_resource secrets {
+  for_each = local.aws_secrets
+  provisioner "shell" {
+    command = [
+      "env | grep ${each.value}"
+    ]
+  }
 }
